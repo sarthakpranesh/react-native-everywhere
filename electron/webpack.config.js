f74642b72d8870e03be8772bea3901d52f4568d9
sarthakpranesh <sarthakpranesh08@gmail.com>
2021-07-02 10:40:48 +0530

    Got this bi*** to work :">
    Signed-off-by: sarthakpranesh <sarthakpranesh08@gmail.com>

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -1,5 +1,6 @@
 const { withExpoWebpack } = require('@expo/electron-adapter');
 
 module.exports = config => {
+  config.resolve.extensions.push('.web.js', '.web.ts', '.web.tsx');
   return withExpoWebpack(config);
 };
